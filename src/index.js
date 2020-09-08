741f6f21256a11cf3958b5170278a6adcd667234
darq37 <dzmudziak@gmail.com>
2020-09-08 12:55:58 +0200

    use react Hooks to handle button state

--- src/index.js
+++ src/index.js
@@ -1,26 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const Hello = (props) => {
+const App = () => {
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+    allClicks: [],
+  });
+
+  const handleLeftClick = () => {
+    setClicks({
+      left: clicks.left + 1,
+      right: clicks.right,
+      allClicks: (clicks.allClicks = clicks.allClicks.concat("L")),
+    });
+  };
+
+  const handleResetCLick = () => {
+    setClicks({
+      left: (clicks.left = 0),
+      right: (clicks.right = 0),
+      allClicks: (clicks.allClicks = []),
+    });
+  };
+
+  const handleRightClick = () => {
+    setClicks({
+      left: clicks.left,
+      right: clicks.right + 1,
+      allClicks: (clicks.allClicks = clicks.allClicks.concat("R")),
+    });
+  };
+
   return (
     <>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      {clicks.left}
+      <Button clickHandler={handleLeftClick} text="left" />
+      <Button clickHandler={handleResetCLick} text="reset" />
+      <Button clickHandler={handleRightClick} text="right" />
+      {clicks.right}
+      <History allClicks ={clicks.allClicks}/>
     </>
   );
 };
 
-const App = () => {
-  const name = "Dominik";
-  const age = 26;
-  return (
-    <>
-      <p>Welcome</p>
-      <Hello name={name} age={age}/>
-    </>
-  );
-};
+const Button = ({ clickHandler, text }) => (
+  <button onClick={clickHandler}>{text}</button>
+);
+
+const History = (props) => {
+  if(props.allClicks.length ===0){
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return(
+    <div>
+      button press history: {props.allClicks.join('>')}
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
