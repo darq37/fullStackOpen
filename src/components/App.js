a987d951d40c88abe0a20b61554365a7047c668b
darq37 <dzmudziak@gmail.com>
2020-09-09 13:21:28 +0200

    add functionality to post notes to Json-server

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Note from "./Note";
+import axios from "axios";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
-  const [showAll, setShowAll] = useState(true)
+  const [showAll, setShowAll] = useState(true);
 
-  const notestoShow = showAll ? notes : notes.filter (note => note.important)
+  const notestoShow = showAll ? notes : notes.filter((note) => note.important);
+
+  useEffect(() => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data);
+    });
+  }, []);
+  console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
     event.preventDefault();
     const noteObj = {
       content: newNote,
-      date: new Date().toISOString(),
+      date: new Date(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     };
-    setNotes(notes.concat(noteObj));
-    setNewNote("");
+    axios.post("http://localhost:3001/notes", noteObj).then((response) => {
+      setNotes(notes.concat(response.data));
+      setNewNote(""); //reseting the note creation form
+    });
   };
 
   const handleNoteChange = (event) => {
@@ -29,7 +40,7 @@ const App = (props) => {
       <h1>Notes</h1>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all' }
+          show {showAll ? "important" : "all"}
         </button>
       </div>
       <ul>
